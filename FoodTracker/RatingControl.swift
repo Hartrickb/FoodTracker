c728bfbf0ba25f459cb063ad4205b10be6978eaa
Hartrickb <Hartrickb@users.noreply.github.com>
2016-12-05 15:44:00 -0500

    feat: added a button (future rating system)

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,34 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Bennett Hartrick on 12/5/16.
+//  Copyright © 2016 Bennett. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+        addSubview(button)
+        
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+}
